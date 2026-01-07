a3e5415a01f4b584c661bba3f9b675e137393638
Kat Nykiel <katnykiel@proton.me>
2026-01-07 13:42:07 -0500

    updated hibernate

--- laptop.nix
+++ laptop.nix
@@ -11,32 +11,25 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
-  # Fix lid closure behavior
-  services.logind.settings.Login = {
-    HandleLidSwitch = "hibernate";
-    HandleLidSwitchExternalPower = "lock";
-    HandleLidSwitchDocked = "ignore";
-  };
-
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     btop
   ];
 
+  # Suspend first then hibernate when closing the lid
+  services.logind.settings.Login.HandlelidSwitch = "suspend-then-hibernate";
+  # Hibernate on power button pressed
+  services.logind.settings.Login.HandlepowerKey = "suspend";
+  services.logind.settings.Login.HandlePowerKeyLongPress = "poweroff";
+
   # Specify encrypted disk partition id
   boot.initrd.luks.devices."luks-198aef4c-3bd7-4efd-bcd9-bbf207d06b4a".device = "/dev/disk/by-uuid/198aef4c-3bd7-4efd-bcd9-bbf207d06b4a";
-
-  # Enable nouveau
-  hardware.graphics = {
-    enable = true;
-  };
-
+  
   # Adds some fonts
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
     nerd-fonts.droid-sans-mono
   ];
 
-}
+}j
